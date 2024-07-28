81f5b80ae6b263499bea2a479201deb1f185a1bb
NathanEKelly <1031929@sae.edu.au>
2024-07-29 03:03:09 +0930

    Implemented enemy targeting of closest waypoints

--- Assets/Scripts/Ships/EnemyShip.cs
+++ Assets/Scripts/Ships/EnemyShip.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyShip : Ship
 {
+    Waypoint[] Waypoint;
+    Waypoint NearestWaypoint;
+
+    float distance;
+    float nearestDistance = 1000f;
+
+    private void Start()
+    {
+        Waypoint = FindObjectsOfType<Waypoint>();
+
+        for (int i = 0; i < Waypoint.Length; i++)
+        {
+            distance = Vector3.Distance(transform.position, Waypoint[i].transform.position);
+            if (distance < nearestDistance)
+            {
+                NearestWaypoint = Waypoint[i];
+                nearestDistance = distance;
+
+                ShipMovement.SetTargetWaypoint(NearestWaypoint);
+            }
+        }
+    }
+
+
 
 }

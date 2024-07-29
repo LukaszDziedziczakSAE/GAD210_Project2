e431f2df84716da8232085899f3d1e3d829f15b1
NathanEKelly <1031929@sae.edu.au>
2024-07-29 13:21:15 +0930

    work on health script

--- Assets/Scripts/Ships/Health.cs
+++ Assets/Scripts/Ships/Health.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health;
+    public float maxHealth;
+
+    public event Action OnDeath;
+    public void TakeDamage(float damage)
     {
-        
+        health -= damage;
+        if(health <= 0)
+        {
+            OnDeath?.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+    
 }
